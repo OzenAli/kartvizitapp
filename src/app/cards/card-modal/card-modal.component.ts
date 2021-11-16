ffe7df4602b3ec53f23c85f5651d610cfa689237
OzenAli <aliozen1993@hotmail.com>
2021-11-16 09:40:06 +0300

    kartvizit uygulama

---
+++ src/app/cards/card-modal/card-modal.component.ts
@@ -0,0 +1,61 @@
+import { Component, Inject, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Card } from 'src/app/models/card';
+import { CardService } from 'src/app/services/card.service';
+
+@Component({
+  selector: 'app-card-modal',
+  templateUrl: './card-modal.component.html',
+  styleUrls: ['./card-modal.component.scss']
+})
+export class CardModalComponent implements OnInit {
+
+  cardForm!: FormGroup;
+
+  constructor(
+    private dialogRef: MatDialogRef<CardModalComponent>,
+    private fb: FormBuilder,
+    private cardService: CardService,
+    private _snackBar: MatSnackBar,
+    @Inject(MAT_DIALOG_DATA) public data:Card
+  ) { }
+
+  ngOnInit(): void {
+    console.log(this.data);
+    this.cardForm = this.fb.group({
+      name: [this.data?.name || '',Validators.maxLength(50)],
+      title: [this.data?.title,[Validators.required,Validators.maxLength(255)]],
+      phone: [this.data?.phone,[Validators.required,Validators.maxLength(20)]],
+      email: [this.data?.email,[Validators.email,Validators.maxLength(50)]],
+      address: [this.data?.address,Validators.maxLength(255)]
+    });
+  }
+
+  addCard(){
+    console.log(this.cardForm.value);
+    this.cardService.addCard(this.cardForm.value).subscribe(res => {
+      console.log(res);
+      this.cardService.getCards();
+      this.dialogRef.close();     
+    })
+  }
+
+  updateCard(){
+    this.cardService.updateCard(this.cardForm.value, this.data.id).subscribe(res => {
+      console.log(res);
+      this.cardService.getCards();
+      this.dialogRef.close();
+    })
+  }
+
+  deleteCard(){
+    this.cardService.deleteCard(this.data.id).subscribe(res => {
+      console.log(res);
+      this.cardService.getCards();
+      this.dialogRef.close();
+    })
+  }
+
+}
